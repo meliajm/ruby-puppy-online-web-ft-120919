38c182e0c630d2afb13365e6794f03510f87eb59
melia miller <meliajm@gmail.com>
2019-12-14 04:08:45 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
 # Add your code here
+# require 'pry'
+# require 'byebug'
+class Dog 
+  @@all = []
+  attr_reader :name
+  
+  
+  def initialize(name)
+    @name = name
+    # @@all << self
+    save
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@all.each do |puppy| 
+      puts puppy.name 
+      # byebug
+    end
+    
+  end 
+  
+  def self.clear_all
+    @@all = []
+  end 
+  
+  def save
+    @@all << self
+  end
+end
+
+
+  
